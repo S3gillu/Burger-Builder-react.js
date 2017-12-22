30351ef9c5bdf81735e3f6ab5b8def696df3eca6
S3gillu <shubhamsaurabh440@yahoo.com>
2017-12-22 18:33:45 +0530

    Authentication first phase implemented

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -22,10 +22,10 @@ export const purchaseBurgerStart = () => {
     };
 };
 
-export const purchaseBurger = (orderData) => {
+export const purchaseBurger = (orderData, token) => {
     return dispatch => {
         dispatch(purchaseBurgerStart());
-        axios.post('/order.json', orderData)
+        axios.post('/order.json?auth='+ token, orderData)
             .then(response => {
                 dispatch(purchaseBurgerSuccess(response.data.name, orderData));
             })
@@ -62,10 +62,10 @@ export const fetchOrderStart = () => {
     };
 };
 
-export const fetchOrders = () => {
+export const fetchOrders = (token) => {
     return dispatch => {
         dispatch(fetchOrderStart());
-        axios.get('/order.json')
+        axios.get('/order.json?auth=' + token)
             .then(res => {
                 const fetchedOrders = [];
                 for (let key in res.data) {
